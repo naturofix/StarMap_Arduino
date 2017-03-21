4921fac1cc767ea93c215ac5aac4259c5f2e0414
Shaun <shaungarnett@gmail.com>
2017-03-21 18:36:44 +0200

    update

--- pyserial-2.7/read_arduino.py
+++ pyserial-2.7/read_arduino.py
@@ -24,6 +24,8 @@ while True:
 		else:
 			serial_dic[read_line_list[0]].append(read_line_list[1].replace('\n',''))
 		if read_line_list[0] in print_list:
+			if read_line_list[0] == print_list[0]:
+				print '\n'
 			print '%s : %s' %(read_line_list[0],read_line_list[1])
 	#print n
 
